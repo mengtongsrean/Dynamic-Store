a644286fcecc2d76bfcb6a2c39ce9c37d514cf73
mengtongsrean <mengtongsrean17@gmail.com>
2024-06-03 17:43:37 +1000

    connect to Vercel for backend hosting

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 // server.js
 const app = require('./app');
 PORT = 8080;
-const port = PORT || 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
